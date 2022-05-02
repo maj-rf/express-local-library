5c97aa765a4fff4c78f678d5ec24530ee35e98ba
bananabread08 <rmkfermin@gmail.com>
2022-05-02 16:26:50 +0800

    add new route to users.js in routes folder

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const { reset } = require('nodemon');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/cool', function (req, res, next) {
+  res.send("You're so cool!");
 });
 
 module.exports = router;
