c9f39207aef7c82a3af631b246ceef9bce1665e7
bananabread08 <rmkfermin@gmail.com>
2022-05-17 15:11:52 +0800

    update bookinstance_list

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,7 +1,19 @@
 const BookInstance = require('../models/bookinstance');
 
-exports.bookinstance_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance list');
+// Display list of all BookInstances.
+exports.bookinstance_list = function (req, res, next) {
+  BookInstance.find()
+    .populate('book')
+    .exec(function (err, list_bookinstances) {
+      if (err) {
+        return next(err);
+      }
+      // Successful, so render
+      res.render('bookinstance_list', {
+        title: 'Book Instance List',
+        bookinstance_list: list_bookinstances,
+      });
+    });
 };
 
 exports.bookinstance_detail = function (req, res) {
