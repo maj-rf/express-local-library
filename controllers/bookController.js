dfd7a8399a7dc62afb5f0b5391a3596589894148
bananabread08 <rmkfermin@gmail.com>
2022-05-17 15:10:26 +0800

    update book_list object

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -34,8 +34,18 @@ exports.index = function (req, res) {
   );
 };
 
-exports.book_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: Book list');
+// Display list of all Books.
+exports.book_list = function (req, res, next) {
+  Book.find({}, 'title author')
+    .sort({ title: 1 })
+    .populate('author')
+    .exec(function (err, list_books) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render('book_list', { title: 'Book List', book_list: list_books });
+    });
 };
 
 exports.book_detail = function (req, res) {
