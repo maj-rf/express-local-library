d64765e32f66abed2456723588eee45c92385fbe
bananabread08 <rmkfermin@gmail.com>
2022-05-18 14:27:01 +0800

    update author_list object to render all authors

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,7 +1,19 @@
 const Author = require('../models/author');
 
-exports.author_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author list');
+// Display list of all Authors.
+exports.author_list = function (req, res, next) {
+  Author.find()
+    .sort([['family_name', 'ascending']])
+    .exec(function (err, list_authors) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render('author_list', {
+        title: 'Author List',
+        author_list: list_authors,
+      });
+    });
 };
 
 exports.author_detail = function (req, res) {
