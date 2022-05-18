be4486fa3a211c08e13c6f3c9a04c362f224d5dc
bananabread08 <rmkfermin@gmail.com>
2022-05-18 15:19:34 +0800

    update bookinstance_detail method to render each of instance detail

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -16,8 +16,26 @@ exports.bookinstance_list = function (req, res, next) {
     });
 };
 
-exports.bookinstance_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+// Display detail page for a specific BookInstance.
+exports.bookinstance_detail = function (req, res, next) {
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) {
+        return next(err);
+      }
+      if (bookinstance == null) {
+        // No results.
+        var err = new Error('Book copy not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      res.render('bookinstance_detail', {
+        title: 'Copy: ' + bookinstance.book.title,
+        bookinstance: bookinstance,
+      });
+    });
 };
 
 exports.bookinstance_create_get = function (req, res) {
