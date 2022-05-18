4b78d4e8850189b42ce4542e1a66115ff5b4de3b
bananabread08 <rmkfermin@gmail.com>
2022-05-18 15:15:07 +0800

    update author_detail method to render all available information like every books authored

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 const Author = require('../models/author');
+const async = require('async');
+const Book = require('../models/book');
 
 // Display list of all Authors.
 exports.author_list = function (req, res, next) {
@@ -16,8 +18,35 @@ exports.author_list = function (req, res, next) {
     });
 };
 
-exports.author_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+// Display detail page for a specific Author.
+exports.author_detail = function (req, res, next) {
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books: function (callback) {
+        Book.find({ author: req.params.id }, 'title summary').exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      } // Error in API usage.
+      if (results.author == null) {
+        // No results.
+        var err = new Error('Author not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      res.render('author_detail', {
+        title: 'Author Detail',
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
 exports.author_create_get = function (req, res) {
