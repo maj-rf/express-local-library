1b5d20f59e115f59a3d05f246c2cfb41ba43f180
bananabread08 <rmkfermin@gmail.com>
2022-05-18 14:56:59 +0800

    add parallel async to genre_detail for rendering said genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 const Genre = require('../models/genre');
+const Book = require('../models/book');
+const async = require('async');
+
 exports.genre_list = function (req, res) {
   Genre.find()
     .sort([['name', 'ascending']])
@@ -14,8 +17,36 @@ exports.genre_list = function (req, res) {
     });
 };
 
-exports.genre_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+// Display detail page for a specific Genre.
+exports.genre_detail = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        var err = new Error('Genre not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render('genre_detail', {
+        title: 'Genre Detail',
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 exports.genre_create_get = function (req, res) {
