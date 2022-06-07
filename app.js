dd7f7c346f9fbda3479fb8e31f196f56d6b617b8
bananabread08 <rmkfermin@gmail.com>
2022-06-07 10:31:02 +0800

    fix code arrangement

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression());
 app.use(helmet());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
