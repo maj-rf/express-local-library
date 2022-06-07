acd93d645febc15f41a3ab7b4f0dfdf986d74f8f
bananabread08 <rmkfermin@gmail.com>
2022-06-07 09:49:14 +0800

    install compression and helmet
    create README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Express Local Library
+
+A tutorial project to learn about NodeJS and Express.
+
+## Learnings and Outcomes
+
+1. NodeJS plus its framework, ExpressJS.
+
+2. The MVC model (Model, View, Controller).
+
+3. Databases (between relational & non-relational DBs) and MongoDB.
+
+4. Views: Basics and techniques about templating engines (Pug).
+
+5. Models: Mongoose + Schemas
+
+6. Controllers: Validation, Routing, Async
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -26,6 +28,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
