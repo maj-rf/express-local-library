d01cbf8a865e24f3dc49f76ab2cae6f03af74ea4
bananabread08 <rmkfermin@gmail.com>
2022-06-07 09:10:35 +0800

    implement update_post for bookinstance when form is valid

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -184,4 +184,62 @@ exports.bookinstance_update_get = function (req, res) {
   );
 };
 
-exports.bookinstance_update_post = function (req, res) {};
+exports.bookinstance_update_post = [
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status').escape(),
+  body('due_back', 'Invalid date')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    var bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values and error messages.
+      Book.find({}, 'title').exec(function (err, books) {
+        if (err) {
+          return next(err);
+        }
+        // Successful, so render.
+        res.render('bookinstance_form', {
+          title: 'Update BookInstance',
+          book_list: books,
+          selected_book: bookinstance.book._id,
+          errors: errors.array(),
+          bookinstance: bookinstance,
+        });
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      BookInstance.findByIdAndUpdate(
+        req.params.id,
+        bookinstance,
+        {},
+        function (err, thebookinstance) {
+          if (err) {
+            return next(err);
+          }
+          //Successful - redirect to the bookinstance detail page.
+          res.redirect(thebookinstance.url);
+        }
+      );
+    }
+  },
+];
